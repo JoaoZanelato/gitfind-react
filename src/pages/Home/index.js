d50357be66e131b5929c4f049b7dda082c7f3a85
JoaoZanelato <jgbzanelato@gmail.com>
2025-09-17 14:34:32 -0300

    profile ready

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,6 +12,21 @@ function App() {
           <input name="usuario" placeholder="@username"/>
           <button>Buscar</button>
         </div>
+        <div className="profile">
+          <img src="https://avatars.githubusercontent.com/u/127459520?v=4" className="profile-pic" alt="profile-pic"/>
+          <div>
+            <h3>
+              João Zanelato
+            </h3>
+            <span>
+              @JoaoZanelato
+            </span>
+            <p>
+              Descrição
+            </p>
+          </div>
+        </div>
+        <hr/>
       </div>
     </div>
   </div>;
